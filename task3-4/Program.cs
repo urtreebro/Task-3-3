c196cd3db81b881151258ea29a58f2c64bca4141
urtreebro <acespadesdamn@bk.ru>
2024-02-05 00:49:53 +0300

    added task 3-2 as a base for 3-4

---
+++ task3-4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayBase[] allArrays = new ArrayBase[3];
+
+            Random rnd = new();
+
+            OneDimensionalArray onedimarray = new();
+            allArrays[0] = onedimarray;
+
+            TwoDimensionalArray twodimarray = new();
+            allArrays[1] = twodimarray;
+
+            JaggedArray jaggedarray = new();
+            allArrays[2] = jaggedarray;
+
+            foreach (ArrayBase array in allArrays)
+            {
+                Console.WriteLine($"Average: {array.FindAverage()} \n");
+
+                array.PrintArray();
+
+                Console.WriteLine();
+
+                array.Refill();
+
+                Console.WriteLine("Refilled array\n");
+
+                array.PrintArray();
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
